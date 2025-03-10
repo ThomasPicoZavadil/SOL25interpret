306833975ef643ffc97659151bf94de526b1ae6a
ThomasPicoZavadil <zavadilthomas@gmail.com>
2025-03-10 17:48:49 +0100

    updated XML tree for parameters

--- parse.py
+++ parse.py
@@ -20,7 +20,8 @@ selector_tail: IDSELECT selector_tail |
 block: "[" block_par "|" block_stat "]"
 block_par: (parameter)*
 parameter: ":" IDDEF
-block_stat: (ID ":=" expr ".")*
+block_stat: (ID assign expr ".")*
+assign: ":=" 
 
 expr: expr_base expr_tail
 expr_tail: ID | expr_sel
@@ -53,27 +54,43 @@ def parse_file(file_path):
 # ✅ **Třída pro převod AST na XML**
 class ASTToXML:
     def __init__(self):
-        self.root = ET.Element('program', language='SOL25', description=' ')
+        self.root = ET.Element("program", language="SOL25", description=" ")
 
-    def convert(self, node):
-        return self._convert_node(node, self.root)
+    def convert(self, ast):
+        self._convert_node(ast, self.root)
+        return self.root
+
 
     def _convert_node(self, node, parent_elem):
         if isinstance(node, str):
-            elem = ET.SubElement(parent_elem, "token")
-            elem.text = node
+            ET.SubElement(parent_elem, "var", {"name": node})
+            return
+
+        if node.data == "class_def":
+            elem = ET.SubElement(parent_elem, "class", name=node.children[0])
+            parent_class = ET.SubElement(elem, "inherits")
+            parent_class.text = node.children[1]
+            for method in node.children[2:]:
+                self._convert_node(method, elem)
             return elem
-        
+
+        if node.data == "block":
+            block_elem = ET.SubElement(parent_elem, "block", arity=str(len(node.children[0].children)))
+            for idx, param in enumerate(node.children[0].children, start=1):
+                ET.SubElement(block_elem, "parameter", name=param.children[0], order=str(idx))
+            for stat in node.children[1:]:
+                self._convert_node(stat, block_elem)
+            return block_elem
+
         elem = ET.SubElement(parent_elem, node.data)
         for child in node.children:
             self._convert_node(child, elem)
 
-        return elem
-
     def to_pretty_xml(self):
-        rough_string = ET.tostring(self.root, encoding='unicode')
+        rough_string = ET.tostring(self.root, encoding="unicode")
         reparsed = minidom.parseString(rough_string)
         return reparsed.toprettyxml(indent="  ")
+
 
 # ✅ **Spuštění programu**
 if __name__ == "__main__":
